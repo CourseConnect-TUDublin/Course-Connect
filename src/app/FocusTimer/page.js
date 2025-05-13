6dc4a6aebe922feace8001038a7ff67619a6840e
dylanmugo <92232341+dylanmugo@users.noreply.github.com>
2025-05-13 23:49:43 +0100

    Focus timer code

---
+++ src/app/FocusTimer/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function FocusTimerPage() {
+  return (
+    <main className="container">
+      <h1>Focus Timer</h1>
+      {/* Timer Component */}
+    </main>
+  );
+}
