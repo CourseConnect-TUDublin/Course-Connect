674a9d7567cf7db51db19addf3f95074ba0b3a8c
Ian Mugo <b00143913@mytudublin.ie>
2025-05-20 21:19:00 +0000

    Navbar update for mobile

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body>
         <Toaster position="top-center" />
         <SplashWrapper>{children}</SplashWrapper>
